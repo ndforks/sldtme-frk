2f8af245e0dde4393d07923d0797de17bf756ba1
Niels Drost <nielsdrost7+github@gmail.com>
2026-03-05 18:45:13 +0100

    initial commit

--- database/migrations/2024_01_20_110439_create_projects_table.php
+++ database/migrations/2024_01_20_110439_create_projects_table.php
@@ -8,29 +8,22 @@ return new class () extends Migration {
     public function up(): void
     {
         Schema::create('projects', static function (Blueprint $table): void {
-            $table->uuid('id')->primary();
+            $table->id();
             $table->string('name', 255);
             $table->string('color', 16);
             $table->integer('billable_rate')->unsigned()->nullable();
             $table->boolean('is_public')->default(false);
-
-            // Billable flag - set based on billable_rate during migration
             $table->boolean('is_billable')->nullable();
-
-            // Archival
             $table->dateTime('archived_at')->nullable();
-
-            // Time tracking
             $table->integer('estimated_time')->unsigned()->nullable();
             $table->bigInteger('spent_time')->unsigned()->default(0);
-
-            $table->uuid('client_id')->nullable();
+            $table->unsignedBigInteger('client_id')->nullable();
             $table->foreign('client_id')
                 ->references('id')
                 ->on('clients')
                 ->cascadeOnUpdate()
                 ->restrictOnDelete();
-            $table->uuid('organization_id');
+            $table->unsignedBigInteger('organization_id');
             $table->foreign('organization_id')
                 ->references('id')
                 ->on('organizations')
